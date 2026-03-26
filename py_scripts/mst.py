a0e4b806d1f1c1e9995dff44f5a6ee8461238400
Yessad Youdas <cofirmation604@gmail.com>
2026-03-26 18:03:09 +0800

    Refactoring into pynb & visualization tools

--- py_scripts/mst.py
+++ py_scripts/mst.py
@@ -3,13 +3,8 @@ import pandas as pd
 import networkx as nx
 import yfinance as yf
 
-window = 60
 
-def mst (returns):
-
-    #OBTAINING DISTANCE MATRIX
-    #correlation matrix (p_ij) (.corr() -> Pearson correlation coefficient)
-    #Pearson used in the Mantegna paper -> Pearson deals with continuous data (while Spearman and Kendall is for ordinal)
+def clipping(returns, window):
     corr_matrix = returns.corr()
 
     # CLIPPING VALUES TO AVOID NUMERICAL INSTABILITIES (MARKET )
@@ -26,37 +21,14 @@ def mst (returns):
     d = np.diag(C_clean)
     inv_std = np.diag(1 / np.sqrt(d))
     C_final = inv_std @ C_clean @ inv_std
+    # Convert back to DataFrame with proper index and columns
+    C_final_df = pd.DataFrame(C_final, index=corr_matrix.index, columns=corr_matrix.columns)
+    return C_final_df
 
-
-    # #corr_matrix -> dist_matrix ( d(i, j ) = 1 − ρ_ij^2 )
-    # #not sure which one to use, I've read online that sqrt(2(1-p)) is favorable, one because it preserves direction, 2. because its euclidian as well
-    # dist_matrix = (1-(corr_matrix ** 2))
-    # dist_matrix_euclidian = np.sqrt(2 * (1 - corr_matrix))
+def mst (clipped_corr_matrix):
 
     #BUILDING MST
-    #graph G, nodes := stocks, edges := dist_matrix entry
-    G = nx.from_pandas_adjacency(corr_matrix) 
+    G = nx.from_pandas_adjacency(clipped_corr_matrix) 
     mst = nx.minimum_spanning_tree(G)
 
     return mst
-
-def rolling_window_mst ():
-    #OBTAINING DATA
-    trees = []
-    tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'NVDA', 'META', 'TSLA', 'AVGO', 'COST', 'PEP']
-    data = yf.download(tickers, start="2023-01-01", progress=False)['Close']
-    #OBTAINING RETURNS
-    #Yi = lnPi(t) − lnPi(t − 1)
-    returns = np.log(data /data.shift(1)).dropna()
-    
-   
-    
-
-    for i in range(window, len(returns)):
-        window_slice = returns.iloc[i-window : i]
-        current_date = returns.index[i]
-        curr_tree = mst(window_slice)
-        trees.append((current_date, curr_tree))
-
-    df_trees = pd.DataFrame(trees, columns=['Date', 'MST'])
-    return df_trees
